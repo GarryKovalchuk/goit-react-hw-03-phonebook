19b4149c91655d5f0eb5bbf04414f0a2151468be
Garry <vorzel47051@gmail.com>
2024-01-12 22:27:38 +0200

    react03phonebook

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -32,7 +32,7 @@ export class ContactForm extends Component {
           onChange={this.handleChange}
           placeholder="Name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          title="Назва може містити лише літери, апостроф, тире та пробіли. Наприклад Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
 
@@ -43,7 +43,7 @@ export class ContactForm extends Component {
           onChange={this.handleChange}
           placeholder="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          title="Номер телефону має складатися з цифр і може містити пробіли, тире, круглі дужки та може починатися з +"
           required
         />
 
